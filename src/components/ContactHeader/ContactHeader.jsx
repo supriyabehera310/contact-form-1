b768572a1d156f226549854073d20046e73b6818
rupaven <you@example.com>
2024-01-13 16:36:28 +0530

    not here to

--- src/components/ContactHeader/ContactHeader.jsx
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -6,7 +6,7 @@ const ContactHeader = () => {
     <div className={`container ${styles.contact_section}`}>
       <h1>CONTACT US</h1>
       <p>
-        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+        LET’S CONNECT: WE’RE not HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
         WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
         REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
         EMAIL, OR SOCIAL MEDIA.{" "}
